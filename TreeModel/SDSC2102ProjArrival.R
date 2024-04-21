8304fee2b9e6a879dec5af3cdf08e20ec0201363
ronineume <127309099+ronineume@users.noreply.github.com>
2024-04-21 18:55:00 +0800

    Add files via upload

---
+++ TreeModel/SDSC2102ProjArrival.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+library(tidymodels)
+library(rpart)
+library(rpart.plot)
+
+df <- read_csv('Dataset/Arrivaldata.csv')
+
+colnames(df)
+
+glimpse(df)
+
+df <- df %>%
+  select(
+    `Mainland Visitors`,
+    MainlandReddays,
+    ExtremeWeather,
+    `Red days`,
+    Weekday,
+    Date
+  ) %>%
+  rename(
+    MV = `Mainland Visitors`,
+    MR = MainlandReddays,
+    EW = ExtremeWeather,
+    RD = `Red days`,
+    WD = Weekday,
+    date = Date
+  )
+
+#split_object <- initial_split(data=df, prop=0.5)
+
+#Splitting the data
+df_train <- df %>%
+  filter(month(date) %in% c(5, 7, 9))
+
+df_test <- df %>%
+  filter(month(date) %in% c(6, 8, 10))
+
+#df_train <- training(split_object)
+#df_test <- testing(split_object)
+
+glimpse(df_train)
+
+glimpse(df_test)
+
+tree <- rpart(MV ~ MR + EW + RD + WD, data = df_train)
+prp(tree)
+rpart.plot(tree, extra = 100, under = FALSE, cex = 1, box.palette = "auto")
+
+tree_predictions <- predict(tree, df_test)
+y_test <- df_test %>% pull(MV)
+cor(tree_predictions, y_test)
+
---
+++ TreeModel/SDSC2102ProjDeparture.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(tidymodels)
+library(rpart)
+library(rpart.plot)
+
+df <- read_csv('Dataset/Departuredata.csv')
+
+colnames(df)
+
+glimpse(df)
+
+df <- df %>%
+  select(
+    `Hong Kong Residents`,
+    MainlandReddays,
+    ExtremeWeather,
+    `Red days`,
+    Weekday,
+    Date
+  ) %>%
+  rename(
+    HK = `Hong Kong Residents`,
+    MR = MainlandReddays,
+    EW = ExtremeWeather,
+    RD = `Red days`,
+    WD = Weekday,
+    date = Date
+  )
+
+#split_object <- initial_split(data=df, prop=0.5)
+
+#Splitting the data
+df_train <- df %>%
+  filter(month(date) %in% c(5, 7, 9))
+
+df_test <- df %>%
+  filter(month(date) %in% c(6, 8, 10))
+
+#df_train <- training(split_object)
+#df_test <- testing(split_object)
+
+glimpse(df_train)
+
+glimpse(df_test)
+
+tree <- rpart(HK ~  EW + RD + WD , data = df_train)
+prp(tree)
+rpart.plot(tree, extra = 100, under = FALSE, cex = 1, box.palette = "auto")
+
+tree_predictions <- predict(tree, df_test)
+y_test <- df_test %>% pull(HK)
+cor(tree_predictions, y_test)
+
+
